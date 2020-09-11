0a9f6955a3a5f21b164e4defeaf23efda752c1f2
The Art Appreciator Art <pvtfreestone1@gmail.com>
2020-09-11 00:47:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*Come up with a strategy before coding.
+n is our limit.
+declare var i=0.
+we increment i by 1.
+add i to the result variable.
+stop when i ===n*/
+
+function sum(n)[
+    var result=0;
+    var i=0;
+
+    while (i<= n){
+        result= result + n;
+        i= i + 1;
+    }
+    return result;
+    }
+]
+//
